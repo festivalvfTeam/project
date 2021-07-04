85591fcd01386eeee74a25bdb4988fd34dd855d8
Dmitry <dima.game2010@yandex.ru>
2021-07-04 18:45:10 +0300

    Добавлен вывод каталога в CSV файл

--- vinoteka-app/src/main/java/ru/vinotekavf/vinotekaapp/services/PositionService.java
+++ vinoteka-app/src/main/java/ru/vinotekavf/vinotekaapp/services/PositionService.java
@@ -1,19 +1,20 @@
 package ru.vinotekavf.vinotekaapp.services;
 
 import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.vinotekavf.vinotekaapp.entities.Position;
 import ru.vinotekavf.vinotekaapp.entities.Provider;
 import ru.vinotekavf.vinotekaapp.repos.PositionRepository;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
+import java.sql.*;
 import java.util.List;
 
 @Service
@@ -21,7 +22,15 @@ public class PositionService {
     @Autowired
     private PositionRepository positionRepository;
 
+    @Value("${spring.datasource.url}")
+    private String URL_DB;
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final String QUERY = "select pr.name, pr.phone, po.maker, po.vendor_code, po.product_name, " +
+            "po.volume, po.release_year, po.price, po.remainder, po.fv_vendor_code, po.fv_product_name " +
+            "from provider pr " +
+            "join posit po on pr.id = po.provider_id";
 
     public void save(Position position) {
         positionRepository.save(position);
@@ -29,6 +38,19 @@ public class PositionService {
 
     public List<Position> findAll() {
         return positionRepository.findAll();
+    }
+
+    public void writeAllPositionsInCSV() {
+        String CSV_FILE_PATH = "src/main/resources/files/data.csv";
+        try (Connection connection = DriverManager.getConnection(URL_DB, "postgres", "5842");
+             Statement stmt = connection.createStatement();
+             CSVWriter writer = new CSVWriter(new OutputStreamWriter(new FileOutputStream(CSV_FILE_PATH), "windows-1251"), ';'))
+        {
+            ResultSet myResultSet = stmt.executeQuery(QUERY);
+            writer.writeAll(myResultSet, true);
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void readCSVAndWriteInDb(String csvFilename, String encoding, Provider provider, Integer[] vendorCodeIndexes, Integer[] productNameIndexes, Integer[] volumeIndexes,
--- vinoteka-app/src/test/java/ru/vinotekavf/vinotekaapp/VinotekaAppApplicationTests.java
+++ vinoteka-app/src/test/java/ru/vinotekavf/vinotekaapp/VinotekaAppApplicationTests.java
@@ -1,13 +1,19 @@
 package ru.vinotekavf.vinotekaapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.vinotekavf.vinotekaapp.services.PositionService;
 
 @SpringBootTest
 class VinotekaAppApplicationTests {
 
+	@Autowired
+	private PositionService positionService;
+
 	@Test
 	void contextLoads() {
+		positionService.writeAllPositionsInCSV();
 	}
 
 }
